1962977ef02b2dce0af613b901ac2094afcc145f
Nikita Marshalkin <marnikitta@gmail.com>
2021-03-04 11:17:35 +0300

    Parallelize binarization

--- sgp/trees/binarizer.py
+++ sgp/trees/binarizer.py
@@ -1,33 +1,44 @@
+from concurrent.futures.thread import ThreadPoolExecutor
+
 import numpy as np
 from sklearn.utils import check_array
 
 
 class Binarizer:
-    def __init__(self, n_bins: int = 64, top_n: int = 100000):
+    def __init__(self,
+                 n_bins: int = 64,
+                 top_n: int = 100000,
+                 n_jobs: int = 8):
         self.n_bins = n_bins
         self.top_n = top_n
+        self.n_jobs = 8
         # self.boundaries = None
 
     def fit(self, X: np.ndarray) -> 'Binarizer':
         X = check_array(X)
-        boundaries = np.zeros((X.shape[1], self.n_bins), np.float64)
-        for f_index in np.arange(X.shape[1]):
-            # noinspection PyTypeChecker
-            bins = np.percentile(X[:self.top_n, f_index],
+
+        def f(f_index: int) -> np.ndarray:
+            return np.percentile(X[:self.top_n, f_index],
                                  np.linspace(0, 100, num=self.n_bins, endpoint=True),
                                  interpolation='lower')
-            boundaries[f_index] = bins
 
-        self.boundaries = boundaries
+        with ThreadPoolExecutor(max_workers=self.n_jobs) as pool:
+            boundaries = pool.map(f, range(X.shape[1]))
+            self.boundaries = np.vstack(list(boundaries))
+
         return self
 
     def transform(self, X: np.ndarray) -> np.ndarray:
         assert self.boundaries is not None
         X = check_array(X)
         assert X.shape[1] == self.boundaries.shape[0]
-        df_bins = np.zeros(X.T.shape, np.int64)
-        for f_index in np.arange(df_bins.shape[0]):
-            df_bins[f_index] = np.digitize(X[:, f_index], self.boundaries[f_index]).astype(np.int64) - 1
+
+        def f(f_index: int) -> np.ndarray:
+            return np.digitize(X[:, f_index], self.boundaries[f_index]).astype(np.int64) - 1
+
+        with ThreadPoolExecutor(max_workers=self.n_jobs) as pool:
+            df_bins = pool.map(f, range(X.shape[1]))
+            df_bins = np.vstack(list(df_bins))
 
         np.clip(df_bins, 0, self.n_bins - 1, out=df_bins)
 
